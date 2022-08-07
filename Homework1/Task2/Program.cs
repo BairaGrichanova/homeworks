7f41a0fdab827d2fec1fbaa5976253680ca0908c
Baya Grichanova <bg.red_line@bk.ru>
2022-08-07 20:02:24 +0300

    Task 2: Data entry - correction 1

--- Homework1/Task2/Program.cs
+++ Homework1/Task2/Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.Write ("Введите значение А: ");
 int a = Convert.ToInt32(Console.ReadLine ());
-Console.Write ("Введите значение А: ");
-int a = Convert.ToInt32(Console.ReadLine ());
+Console.Write ("Введите значение Б: ");
+int b = Convert.ToInt32(Console.ReadLine ());
