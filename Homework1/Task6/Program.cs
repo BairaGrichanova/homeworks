06992ce8568a3c1d709f29a27d486ef0ead51450
Baya Grichanova <bg.red_line@bk.ru>
2022-08-07 20:49:06 +0300

    Task 6: Add Data entry

--- Homework1/Task6/Program.cs
+++ Homework1/Task6/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write ("Введите любое число: ");
+double max = Convert.ToDouble (Console.ReadLine ());
