6513fac045aed8baf42285d7d1398eb71c8e3663
Baya Grichanova <bg.red_line@bk.ru>
2022-08-07 20:00:58 +0300

    Task 2: Data entry

--- Homework1/Task2/Program.cs
+++ Homework1/Task2/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write ("Введите значение А: ");
+int a = Convert.ToInt32(Console.ReadLine ());
+Console.Write ("Введите значение А: ");
+int a = Convert.ToInt32(Console.ReadLine ());
