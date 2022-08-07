adbf9d49274bf0caa4523f59ab4e1a4a31707cf6
Baya Grichanova <bg.red_line@bk.ru>
2022-08-07 21:28:46 +0300

    Task 8: Add Formatting Code

--- Homework1/Task8/Program.cs
+++ Homework1/Task8/Program.cs
@@ -1,10 +1,11 @@
 ﻿Console.Write ("Введите целое число N: ");
 int num = Convert.ToInt32 (Console.ReadLine ());
-int count=1, check=0;
+int count = 1, 
+    check = 0;
 Console.Write ("Все чётные числа от 1 до N: ");
 while (num>count-1) 
 {
-    check=count%2;
-        if (check==0) Console.Write (count+" ");
+    check = count % 2;
+        if ( check == 0 ) Console.Write (count+" ");
     count++;
 }
