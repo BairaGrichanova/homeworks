83fccd27db0e491b94290a4062f76860567422f8
Baya Grichanova <bg.red_line@bk.ru>
2022-08-07 21:22:58 +0300

    Task 8: Add While - correction 1

--- Homework1/Task8/Program.cs
+++ Homework1/Task8/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write ("Введите целое число N: ");
 int num = Convert.ToInt32 (Console.ReadLine ());
-int count=0, check=0;
-while (Cont<num+1) 
+int count=1, check=0;
+while (num>count) 
 {
-    check=count%2;
+    count++;
 }
