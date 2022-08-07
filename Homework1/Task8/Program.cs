63391dcf1be3ddf7dd9a27ecb05896c3f95ebe39
Baya Grichanova <bg.red_line@bk.ru>
2022-08-07 21:07:44 +0300

    Task 8: Add Data entry

--- Homework1/Task8/Program.cs
+++ Homework1/Task8/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write ("Введите целое число N: ");
+int num = Convert.ToInt32 (Console.ReadLine ());
