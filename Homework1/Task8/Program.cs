5cf5a5b9d49508d979a91ac801ecf9319e2451aa
Baya Grichanova <bg.red_line@bk.ru>
2022-08-07 21:25:18 +0300

    Task 8: Add Checked Even - correction 1

--- Homework1/Task8/Program.cs
+++ Homework1/Task8/Program.cs
@@ -1,7 +1,10 @@
 ﻿Console.Write ("Введите целое число N: ");
 int num = Convert.ToInt32 (Console.ReadLine ());
 int count=1, check=0;
-while (num>count) 
+
+while (num>count-1) 
 {
+    check=count%2;
+
     count++;
 }
