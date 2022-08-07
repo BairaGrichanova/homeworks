ca7515d956d8623c7ecded0d61c1df22f43b2105
Baya Grichanova <bg.red_line@bk.ru>
2022-08-07 20:26:59 +0300

    Task 2: Formatting code

--- Homework1/Task2/Program.cs
+++ Homework1/Task2/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.Write ("Введите значение А: ");
-int a = Convert.ToInt32(Console.ReadLine ());
+int a = Convert.ToInt32 (Console.ReadLine ());
 Console.Write ("Введите значение Б: ");
-int b = Convert.ToInt32(Console.ReadLine ());
+int b = Convert.ToInt32 (Console.ReadLine ());
 
-if (a>b) Console.Write ("Число "+a+" больше, чем число "+b);
-if (a<b) Console.Write ("Число "+a+" меньше, чем число "+b);
-if (a==b) Console.Write ("Число "+a+" равно числу "+b);
+if ( a > b )  Console.Write ("Число "+a+" больше, чем число "+b);
+if ( a < b )  Console.Write ("Число "+a+" меньше, чем число "+b);
+if ( a == b ) Console.Write ("Число "+a+" равно числу "+b);
