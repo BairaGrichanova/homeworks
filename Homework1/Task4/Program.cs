17327eae2eea122a870c6d21051a61f7e30d290b
Baya Grichanova <bg.red_line@bk.ru>
2022-08-07 20:39:48 +0300

    Task 4: Add IF and Find max

--- Homework1/Task4/Program.cs
+++ Homework1/Task4/Program.cs
@@ -4,3 +4,5 @@ Console.Write ("Введите значение Б: ");
 int b = Convert.ToInt32 (Console.ReadLine ());
 Console.Write ("Введите значение В: ");
 int c = Convert.ToInt32 (Console.ReadLine ());
+if (b>max) max=b;
+if (c>max) max=c;
