fc5d10478c673d4082cd1bbace381b92a622260c
Baya Grichanova <bg.red_line@bk.ru>
2022-08-07 20:25:32 +0300

    Task 2: Data Output

--- Homework1/Task2/Program.cs
+++ Homework1/Task2/Program.cs
@@ -3,6 +3,6 @@ int a = Convert.ToInt32(Console.ReadLine ());
 Console.Write ("Введите значение Б: ");
 int b = Convert.ToInt32(Console.ReadLine ());
 
-if (a>b) 
-if (a<b) 
-if (a==b) 
+if (a>b) Console.Write ("Число "+a+" больше, чем число "+b);
+if (a<b) Console.Write ("Число "+a+" меньше, чем число "+b);
+if (a==b) Console.Write ("Число "+a+" равно числу "+b);
