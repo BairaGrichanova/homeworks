acb3822669854de0fb6c4cbae58da0b370bc0ef1
Baya Grichanova <bg.red_line@bk.ru>
2022-08-07 21:02:42 +0300

    Task 6: Add Data output

--- Homework1/Task6/Program.cs
+++ Homework1/Task6/Program.cs
@@ -1,11 +1,5 @@
 ﻿Console.Write ("Введите целое число: ");
 int num = Convert.ToInt32 (Console.ReadLine ());
 int even = num%2;
-
-
-
-
-
-
-
-Console.Write (even);
+if (even==0) Console.Write ("Вы ввели чётное число");
+else         Console.Write ("Вы ввели нечётное число");
